b092ed45e1fd8eea073905c2bb7c8c4feabe985d
Derick Vigne <derickvigne@me.com>
2019-06-24 22:27:43 -0400

    Added Migrations and Models

--- app/User.php
+++ app/User.php
@@ -5,18 +5,27 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Str;
 
 class User extends Authenticatable
 {
     use Notifiable;
 
+    public $incrementing = false;
+
+    protected static function boot(){
+      parent::boot();
+      static::creating(function ($model) {
+          $model->{$model->getKeyName()} = (string) Str::uuid();
+      });
+    }
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'id', 'first', 'last', 'email', 'password',
     ];
 
     /**
